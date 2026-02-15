1f0d3bf3fad93ebdff6890e0e6a9d3fcb2ba8968
eithansegall <tudshi@gmail.com>
2026-02-15 17:27:32 +0200

    yo

--- src/common/core.py
+++ src/common/core.py
@@ -21,4 +21,3 @@ def countTime(func):
 def printNoNewLine(*args, **kwargs):
     """Prints the given arguments without adding a newline at the end."""
     print(*args, end='', **kwargs)
-    print("hii!")
