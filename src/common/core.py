652f4e2d13ac372fb0dbc78d51edd1fdc34406cb
eithansegall <tudshi@gmail.com>
2026-02-15 18:38:09 +0200

    יו יו

--- src/common/core.py
+++ src/common/core.py
@@ -30,8 +30,3 @@ def is_valid_email(email):
     pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     return re.match(pattern, email) is not None
 
-def test(mail):
-    if is_valid_email(mail):
-        print("Valid email address.")
-    else:
-        print("Invalid email address.")
